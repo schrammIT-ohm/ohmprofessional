f9fd3f70ce6558af59979a5c9595c27f484157b5
LeaHufnagl <lea.hufnagl@gmx.de>
2016-12-07 15:48:46 +0100

    gigik

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
 
-  <h1>Hallo Welt</h1>
+  <h1>Hallo Welt :)</h1>
+  <p>Miau</p>
+
 
 <?php get_footer(); ?>
+Wir ändern was und testen es, damit du siehst, wie es funktioniert
