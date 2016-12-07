18310eaf4e57effa6faca667e8e74de7919a4c8c
WebChromium <chris.chromiumcc@gmail.com>
2016-12-07 15:20:06 +0100

    dö

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
 
   <h1>Hallo Welt Test Leupold Nummer 2</h1>
-<p>Dies ist der erste Pararfggrlhlohkjhkhkaph_0</p>
+<p>Dies ist der erste Paragraph. Dieser Teckst ist valsch</p>
 <?php get_footer(); ?>
