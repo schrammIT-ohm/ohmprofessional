0a83c18f90d7ab7de5a13f0d16bb137bab3066ff
Sebastian Scherer <scheri@roam228163.roam.fh-nuernberg.de>
2016-12-07 18:36:34 +0100

    2. Veranderung jooo

--- index.php
+++ index.php
@@ -4,4 +4,9 @@
   <span> Sersi bersi, matze</span>
   <?php echo "was geht ab?"?>
   <h1>ABC ZDF IHK BLA BLA BLA</h1>
+
+  <img src="http://www.urlaubspiraten.de/media/images/2015/03/swimming-pigs-bahamaspng-1427801575-UCSk-medium.jpg" alt="" />
+  <p>
+    was geht ab?
+  </p>
 <?php get_footer(); ?>
