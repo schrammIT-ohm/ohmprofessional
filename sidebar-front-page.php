0aa31570936dd736e493a4a1a5a2cadcfbdf1964
Mirko Schramm <mirko@Mirkos-MBP.fritz.box>
2016-12-13 22:12:50 +0100

    sidebar: integrated seminare-loop (loop all)

--- sidebar-front-page.php
+++ sidebar-front-page.php
@@ -3,6 +3,35 @@
   <?php get_search_form(); ?>
   <!-- /Suche -->
 
+  <?php
+    // =======================================================================//
+    // ! Seminare
+    // =======================================================================//
+
+    // get all (-1) custom post types "seminare"
+    $args = array(
+      'post_type' => 'seminare',
+      'posts_per_page' => -1
+    );
+    $seminare = new WP_Query( $args );
+
+    // -- nothing found, return false;
+    if (!have_posts()) {
+      return false;
+    }
+   ?>
+
+   <?php while ( $seminare->have_posts() ) : $seminare->the_post(); ?>
+      <a href="<?php the_permalink(); ?>">
+        <h2><?php echo get_the_title();  ?></h2>
+      </a>
+
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+
+
+
+
   <?php if (is_active_sidebar('sidebar-frontpage')) : ?>
       <!-- Widget-Area -->
       <div id="widget-area" class="widget-area">
