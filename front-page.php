84e542ec9e20b527148544abd3dc1b577f89d2a8
Marcel Proff <marcelproff@roam225035.roam.fh-nuernberg.de>
2016-12-14 15:33:42 +0100

    New Version

--- front-page.php
+++ front-page.php
@@ -23,14 +23,31 @@
             ?>
             <img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>">
           </div>
-          <div class="col-sm-10"></div>
+          <div class="col-sm-10">
+          <ul class="list-inline pull-right social-icons">
+            <li> <a href="https://www.facebook.com/"
+              target="_blank"><img src="<?php echo get_bloginfo('template_directory') ?>/img/online_social_media_facebook-128.png" alt="" /></a></li>
+              <li><a href="https://twitter.com/?lang=de"
+                target="_blank"><img src="<?php echo get_bloginfo('template_directory') ?>/img/1481745744_twitter_online_social_media.png" alt="" </a></li>
+            <li> <a href="https://www.xing.com/?ace=sem6ef37eae"
+              target="_blank"><img src="<?php echo get_bloginfo('template_directory') ?>/img/social_media_logo_xing-128.png" alt="" /></a></li>
+            <li> <a href="https://www.linkedin.com/"
+              target="_blank"><img src="<?php echo get_bloginfo('template_directory') ?>/img/online_social_media_linked_in-128.png" alt="" </a></li>
+
+          </ul>
         </div>
       </header>
       <main>
         <h1><?php echo get_bloginfo('name'); ?></h1>
         <h2><?php echo get_bloginfo('description'); ?></h2>
       </main>
-      <footer></footer>
+      <footer><ul>
+        <li>Impressum</li>
+        <li>Datenschutz</li>
+        <li>AGB</li>
+        <li>Kontakt</li>
+      </ul>
+      </footer>
     </div>
 
     <?php // --- Sidebar for FrontPage: https://developer.wordpress.org/reference/functions/get_sidebar/?>
