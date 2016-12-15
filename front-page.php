5b35b56d8d89c794a09861929bc25d34a2434c36
Wikinger93 <a_straubinger@hotmail.de>
2016-12-15 21:29:04 +0100

    Social-Media-Icons: eingefügt, Größe angepasst und ausgerichtet, Footer: Listeneinträge verlinkt, Linkfarbe geändert, ausgerichtet

--- front-page.php
+++ front-page.php
@@ -26,9 +26,13 @@
           <div class="col-sm-10">
           <ul class="list-inline text-right">
             <li><a href="https://de-de.facebook.com/" target="_blank">
-              <img src="<?php echo get_bloginfo('template_directory')?>/img\online_social_media_facebook128.png" alt=""></a></li>
-            <li><a href="https://www.xing.com/" target="_blank">Xing</a></li>
-            <li><a href="https://twitter.com/?lang=de" target="_blank">Twitter</a></li>
+              <img src="<?php echo get_bloginfo('template_directory')?>/img\online_social_media_facebook-128.png" alt="Facebook-Link" width="40px"></a></li>
+            <li><a href="https://twitter.com/?lang=de" target="_blank">
+              <img src="<?php echo get_bloginfo('template_directory')?>/img\1481745744_twitter_online_social_media.png" alt="Twitter-Link" width="40px"></a></li>
+            <li><a href="https://www.xing.com/" target="_blank">
+              <img src="<?php echo get_bloginfo('template_directory')?>/img\social_media_logo_xing-128.png" alt="Xing-Link" width="40px"></a></li>
+            <li><a href="https://de.linkedin.com/" target="_blank">
+              <img src="<?php echo get_bloginfo('template_directory')?>/img\online_social_media_linked_in-128.png" alt="Likedin-Link" width="40px"></a></li>
           </ul>
           </div>
         </div>
@@ -37,12 +41,12 @@
         <h1><?php echo get_bloginfo('name'); ?></h1>
         <h2><?php echo get_bloginfo('description'); ?></h2>
       </main>
-      <footer>
-        <ul class="list-inline">
-          <li>Impressum</li>
-          <li>Datenschutz</li>
-          <li>AGB</li>
-          <li>Kontakt</li>
+      <footer class="footer navbar-fixed-bottom">
+        <ul class="list-inline" margin-left:"10px">
+          <li><a href="#" target="_blank">Impressum</li>
+          <li><a href="#" target="_blank">Datenschutz</li>
+          <li><a href="#" target="_blank">AGB</li>
+          <li><a href="#" target="_blank">Kontakt</li>
         </ul>
       </footer>
     </div>
