39cfa198bcd106a953deb948195325d91d7943f9
spawnYzn <mail@ckoller.de>
2016-12-20 19:42:38 +0100

    Removed / Commented footer content out and moved it to footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
   <?php wp_footer(); ?>
+
+  <footer class="navbar-fixed-bottom">
+          <div class="col-sm-10">
+           <ul class="list-inline">
+              <li><a href="#" target="_blank">Impressum</a></li>
+              <li><a href="#" target="_blank">Datenschutz</a></li>
+              <li><a href="#" target="_blank">AGB</a></li>
+              <li><a href="#" target="_blank">Kontakt</a></li>
+            </ul>
+          </div>
+        </footer>
   </body>
 </html>
