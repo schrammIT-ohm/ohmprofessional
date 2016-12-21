2f773ad44df8fedc787303aa5f4041b5b89c8296
LeaHufnagl <lea.hufnagl@gmx.de>
2016-12-21 14:34:50 +0100

    icons+footer added

--- front-page.php
+++ front-page.php
@@ -19,17 +19,29 @@
               // --- Logo
               // if site-icons is defined: Use the uploaded site icon (Admin->Customizer->Website Infos ->Website Icon)
               // otherwise use default Logo from ../img/...
-              $logoUrl = has_site_icon() ? get_site_icon_url() : get_bloginfo('template_directory') . '/img/ohm-logo.png';
-            ?>
-            <img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>">
+?>
+  <?php get_header(); ?>
+    <!-- FullScreen Wrapper: content + aside -->
+                <div class="fullscreen-wrapper">
+                  <div class="home-content">
+                    <header>
+                      <div class="row">
+                        <div class="col-sm-2">
+                          <?php
+                            // --- Logo
+                            // if site-icons is defined: Use the uploaded site icon (Admin->Customizer->Website Infos ->Website Icon)
+                            // otherwise use default Logo from ../img/...
+                            $logoUrl = has_site_icon() ? get_site_icon_url() : get_bloginfo('template_directory') . '/img/ohm-logo.png';
+                          ?>
+          <img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>">
           </div>
           <div class="col-sm-10"></div>
           <ul class="list-inline pull-right social-icons">
-            <li><a href="https://www.facebook.com/"
+              <li><a href="https://www.facebook.com/"
               target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/facebook.png" alt="" <a/></li>
-            <li><a href="https://www.xing.com/"
+              <li><a href="https://www.xing.com/"
               target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/xing.png" alt="" <a/></li>
-            <li><a href="https://twitter.com/?lang=de"
+              <li><a href="https://twitter.com/?lang=de"
               target= "_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/günther.png" alt="" <a/></li>
             </ul>
         </div>
