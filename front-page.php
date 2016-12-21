137a87a902afb426a372db3b3458f1855ce5456e
Bernhard S <Bernhard S>
2016-12-21 02:32:28 +0100

    Alternative Social Media Icons.

--- front-page.php
+++ front-page.php
@@ -14,24 +14,25 @@
     <div class="home-content">
       <header>
         <div class="row">
-          <div class="col-sm-2">
-            <div class="logo">
-
-
+          <div class="col-sm-6">
             <?php
               // --- Logo
               // if site-icons is defined: Use the uploaded site icon (Admin->Customizer->Website Infos ->Website Icon)
               // otherwise use default Logo from ../img/...
-              $logoUrl = has_site_icon() ? get_site_icon_url() : get_bloginfo('template_directory') . '/img/ohm-logo.png';
+              $logoUrl = has_site_icon() ? get_site_icon_url() : get_bloginfo('template_directory') . '/img/OPS_transparent.png';
             ?>
             <img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>">
           </div>
-          </div>
-          <div class="col-sm-10">
+          <div class="col-sm-6">
             <ul class="list-inline pull-right social-icons">
-              <li><a href ="https://www.facebook.com/" target="_blank"><img src="<?php echo get_bloginfo ('template_directory') ?>/img/online_social_media_facebook-128.png" alt=""></a></li>
-              <li><a href ="https://twitter.com/" target="_blank"><img src="<?php echo get_bloginfo ('template_directory') ?>/img/1481745744_twitter_online_social_media.png" alt=""></a></li>
+              <!--<li><a href="https://www.facebook.com/" target="_blank"><img src="<?php echo get_bloginfo ('template_directory') ?>/img/online_social_media_facebook-128.png" alt=""></a></li>
+              <li><a href="https://twitter.com/" target="_blank"><img src="<?php echo get_bloginfo ('template_directory') ?>/img/1481745744_twitter_online_social_media.png" alt=""></a></li>
               <li><a href="https://www.xing.com/" target="_blank"><img src="<?php echo get_bloginfo ('template_directory') ?>/img/social_media_logo_xing-128.png" alt=""></a></li>
+              <li><a href="https://de.linkedin.com/" target="_blank"><img src="<?php echo get_bloginfo ('template_directory') ?>/img/online_social_media_linked_in-128.png" alt=""></a></li>-->
+              <li><a href="https://www.facebook.com/" target="_blank"><img src="<?php echo get_bloginfo ('template_directory') ?>/img/fb_icon_325x325.png" alt=""></a></li>
+              <li><a href="https://twitter.com/" target="_blank"><img src="<?php echo get_bloginfo ('template_directory') ?>/img/twitter-512.png" alt=""></a></li>
+              <li><a href="https://www.xing.com/" target="_blank"><img src="<?php echo get_bloginfo ('template_directory') ?>/img/xing-icon.png" alt=""></a></li>
+              <li><a href="https://de.linkedin.com/" target="_blank"><img src="<?php echo get_bloginfo ('template_directory') ?>/img/LinkedIn_logo_initials.png" alt=""></a></li>
             </ul>
           </div>
         </div>
