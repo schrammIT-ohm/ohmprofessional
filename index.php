20e28c8eb6b1afc8e56cf4100b2c2de2e6eb8019
Bernhard S <Bernhard S>
2016-12-21 15:15:53 +0100

    Navbar angepasst.

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
  <?php get_header(); ?>
-
+ <?php
+if ( have_posts() ) {
+ while ( have_posts() ) {
+   the_post();
+   //
+   // Post Content here
+   //
+ } // end while
+} // end if
+?>
   <h1>Hallo Welt</h1>
   <p>Blubdiblub</p>
 
