7f2d24781001111475ad5cbe9f9de07fa97a320f
andi-ohm <dommelan52458@th-nuernberg.de>
2016-12-23 22:06:17 +0100

    Add files via upload
    revised front-page

---
+++ front-page.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * See: https://codex.wordpress.org/Theme_Development#Template_Files
+ * - "index.php" is a fallback template only, in case no appropriate template was found
+ * - "front-page.php" is used for the landing-page
+ * - "home.php" is used for the blog (a listing of recent posts)
+ */
+
+?>
+
+<?php get_header(); ?>
+  <!-- FullScreen Wrapper: content + aside -->
+  <div class="fullscreen-wrapper">
+    <div class="home-content">
+      <header>
+        <div class="row">
+          <div class="col-sm-2">
+            <?php
+              // --- Logo
+              // if site-icons is defined: Use the uploaded site icon (Admin->Customizer->Website Infos ->Website Icon)
+              // otherwise use default Logo from ../img/...
+              $logoUrl = has_site_icon() ? get_site_icon_url() : get_bloginfo('template_directory') . '/img/ohm-logo.png';
+            ?>
+            <!-- Bei Klick auf Ohm-Logo immer auf die Landing-Page weiterleiten -->
+            <a href="<?php echo get_bloginfo('template_directory');?>" alt="Startseite"><img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>"></a>
+          </div>
+          <div class="col-sm-10">
+          <ul class="list-inline pull-right social-icons">
+            <li><a href="https://www.facebook.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/online_social_media_facebook-128.png" alt="Facebook" /></a></li>
+            <li><a href="https://www.xing.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/social_media_logo_xing-128.png" alt="Xing" /></a></li>
+            <li><a href="https://www.twitter.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/1481745744_twitter_online_social_media.png" alt="Twitter" /></a></a></li>
+            <li><a href="https://de.linkedin.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory') ?>/img/online_social_media_linked_in-128.png" alt="Linkedin" /></a></li>
+          </ul>
+        </div>
+        </div>
+      </header>
+      <main>
+        <h1><?php echo get_bloginfo('name'); ?></h1>
+        <h2><?php echo get_bloginfo('description'); ?></h2>
+        <a href="<?php echo get_bloginfo('template_directory');?>/about.html">
+        <div class="about">
+          Über uns
+        </div>
+      </main>
+      <footer>
+          <ul class="list-inline">
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/impressum.html">Impressum</a></li>
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/datenschutz.html">Datenschutz</a></li>
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/agb.html">AGB</a></li>
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/kontakt.html">Kontakt</a></li>
+          </ul>
+      </footer>
+    </div>
+
+    <?php // --- Sidebar for FrontPage: https://developer.wordpress.org/reference/functions/get_sidebar/?>
+    <?php get_sidebar('front-page'); ?>
+
+  </div>
+  <!-- /FullScreen Wrapper: content + aside -->
+
+  <!-- FullScreen Image: image + gradient -->
+  <div class="bgimage-container gradient">
+    <img src="<?php echo get_bloginfo('template_directory');?>/img/p_th_hochschulstandort_stadtansicht.jpg" alt="..">
+  </div>
+  <!-- /FullScreen Image: image + gradient -->
+
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
   <h1>Hallo Welt</h1>
+  <p>bla</p>
 
 <?php get_footer(); ?>
+
+<?php
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+		//
+		// Post Content here
+		//
+	} // end while
+} // end if
+?>
