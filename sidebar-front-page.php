90bda72edef78a74728323ef0f4745a3850bf32a
Meemo1307 <13kwagner07@gmail.com>
2016-12-26 02:53:57 +0100

    Alle Änderungen nun mit bootstrap-Klassen!

--- sidebar-front-page.php
+++ sidebar-front-page.php
@@ -30,12 +30,49 @@
       <?php wp_reset_postdata(); ?>
     <?php endif; ?>
 
+<div class="navbar">
+  <div class="navbar-inner">
+<ul class="list-unstyled Menüzwei pull-right">
+  <li><a href="404.php" target="blank" alt="Agil-Management">Agil-Management</a></li>
+  <li><a href="404.php" target="blank" alt="Design Thinking">Design Thinking</a></li>
+  <li><a href="404.php" target="blank" alt="Führen Virtueller Teams">Führen Virtueller Teams</a></li>
+  <li><a href="404.php" target="blank" alt="Innovationskommunikation">Innovationskommunikation</a></li>
+  <li><a href="404.php" target="blank" alt="Open Innovation">Open Innovation</a></li>
+</ul>
+</div>
+</div>
+
+<div class="row">
+<div class="box">
+<div class="Text">
+  <h3> Breaking News: Lorem <h3>
+    <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor sit <br> <a class=pull-right href="404.php"> Weiterlesen </a></p>
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="box">
+<div class="Text">
+  <h3> Breaking News: Lorem <h3>
+    <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor sit <br> <a class=pull-right href="404.php"> Weiterlesen </a></p>
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="box">
+<div class="Text">
+  <h3> Breaking News: Lorem <h3>
+    <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Lorem ipsum dolor sit <br> <a class=pull-right href="404.php"> Weiterlesen </a></p>
+</div>
+</div>
+</div>
 
   <?php
     // =======================================================================//
     // ! Posts
     // =======================================================================//
-
     // -- Get last 3 Posts
     $posts_per_page = 3;
     $args = array(
