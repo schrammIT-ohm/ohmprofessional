c6c7f69095cbfe11c5d02adeb9616201bae8d06f
Manu <Manu@manus-mbp-2.speedport_w_723v_1_41_000>
2016-12-28 13:40:48 +0100

    Social icons eingefügt und Größe angepasst

--- front-page.php
+++ front-page.php
@@ -5,15 +5,13 @@
  * - "front-page.php" is used for the landing-page
  * - "home.php" is used for the blog (a listing of recent posts)
  */
-
 ?>
 
 <?php get_header(); ?>
   <!-- FullScreen Wrapper: content + aside -->
   <div class="fullscreen-wrapper">
     <div class="home-content">
-      <header>
-        <div class="row">
+      <header class="row header">
           <div class="col-sm-2">
             <?php
               // --- Logo
@@ -24,21 +22,27 @@
             <img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>">
           </div>
           <div class="col-sm-10">
-            <ul class="list-inline pull-right social-icons">
-  <li>
-    <a href="https://www.facebook.com/"
-    target="_blank"><img src="<?php echo get_bloginfo('template_directory') ?>/img/online_social_media_facebook-128.png" alt="" /></a></li>
-  <li>twitter</li>
-  <li>xing</li>
+            <ul class="list-inline text-right">
+            <li><a href="https://www.facebook.com" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/online_social_media_facebook-128.png" alt="Facebook-Link" width="40px"></a></li>
+            <li><a href="https://twitter.com/?lang=de" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/1481745744_twitter_online_social_media.png" alt="Twitter-Link" width="40px"></a></li>
+            <li><a href="https://www.xing.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/social_media_logo_xing-128.png" alt="Xing-Link" width="40px"></a></li>
+            <li><a href="https://de.linkedin.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img\online_social_media_linked_in-128.png" alt="Likedin-Link" width="40px"></a
             </ul>
           </div>
-        </div>
       </header>
       <main>
         <h1><?php echo get_bloginfo('name'); ?></h1>
         <h2><?php echo get_bloginfo('description'); ?></h2>
+
       </main>
-      <footer></footer>
+      <footer class="footer navbar-fixed-bottom">
+          <ul class="list-inline" "margin-left:10px">
+            <li><a href="#" target="_blank">Impressum</li>
+            <li><a href="#" target="_blank">Datenschutz</li>
+            <li><a href="#" target="_blank">AGB</li>
+            <li><a href="#" target="_blank">Kontakt</li>
+          </ul>
+      </footer>
     </div>
 
     <?php // --- Sidebar for FrontPage: https://developer.wordpress.org/reference/functions/get_sidebar/?>
