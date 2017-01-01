add9f0ea9ffc778ebae91ed71a5c28ea510d4429
Manu <Manu@manus-mbp-2.speedport_w_723v_1_42_000>
2017-01-01 15:21:56 +0100

    Search button hinzugefügt

--- searchform.php
+++ searchform.php
@@ -4,6 +4,7 @@
 ?>
 <div>
     <form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
+      <img src="<?php echo get_bloginfo('template_directory')?>/img/search-button-without-text-hi.png" alt="" width="40px"/>
         <input placeholder="Suchbegriff eingeben" type="text" value="" name="s">
         <input type="submit" value="">
     </form>
