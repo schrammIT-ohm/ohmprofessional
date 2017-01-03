b8df9f7300a88fc397f51e229f5f886364b4e938
Phil <chris.chromiumcc@gmail.com>
2017-01-03 13:56:40 +0100

    modified 404.php

--- 404.php
+++ 404.php
@@ -1,10 +1,47 @@
 <?php
-  // Achtung: der hier dargestellte Text wurde von PC-Games geklaut -- bitte unbedingt ändern!
+/**
+ * The template for displaying 404 pages (Not Found).
+**/
 
-?>
-<?php get_header(); ?>
-<h1>404 Fehler</h1>
-<h2>Es ist nicht Deine Schuld.</h2>
-<h2>Es ist das Internet.</h2>
-<p>Die aufgerufene Seite konnte leider nicht gefunden werden.</p>
+get_header(); ?>
+<div id="bg">
+     <img src="<?php echo get_bloginfo('template_directory'); ?> \img\Free-Chalkboard-Backgrounds.jpg" alt="background">
+</div>
+<header class="entry-header">
+<center>
+<h1 class="entry-title">404 - Not found</h1>
+</center>
+</header>
+
+<article id="post-0" class="post error404 no-results not-found">
+<div class="entry-content">
+<p><big>404 - Die Seite konnte nicht gefunden werden.</big></p>
+            <?php get_search_form(); ?>
+            
+          <!--   <img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>">  -->
+          <center><img src="<?php echo get_bloginfo('template_directory'); ?> \img\internet-error-404-file-not-found.jpg" alt="404 not-found"  />
+          </center>
+        </div>
+<!-- .entry-content -->
+    </article>
+<!-- #post-0 -->
+<footer>
+
+
+   <ul class="list-inline Impressum">
+     <li> <a href="https://www.th-nuernberg.de/seitenbaum/impressum/page.html" target="blank">
+       <alt=>Impressum</a> </li>
+       <li> |
+       </li>
+     <li> <a href="https://www.th-nuernberg.de/institutionen/hochschulkommunikation/ohm-shop/agb/page.html" target="blank">
+       <alt=>AGB</a> </li>
+       <li> |
+       </li>
+     <li> <a href="https://www.th-nuernberg.de/institutionen/language-center/ueber-uns/page.html" target="blank">
+       <alt=>Über uns</a> </li>
+   </ul>
+
+
+</footer>
+
 <?php get_footer(); ?>
--- front-page.php
+++ front-page.php
@@ -12,58 +12,40 @@
   <!-- FullScreen Wrapper: content + aside -->
   <div class="fullscreen-wrapper">
     <div class="home-content">
-      <header>
-        <div class="row">
+      <header class="row header">
           <div class="col-sm-2">
             <?php
               // --- Logo
               // if site-icons is defined: Use the uploaded site icon (Admin->Customizer->Website Infos ->Website Icon)
               // otherwise use default Logo from ../img/...
-              $logoUrl = has_site_icon() ? get_site_icon_url() : get_bloginfo('template_directory') . '/img/TH_Nuernberg_logo.jpg';
+              $logoUrl = has_site_icon() ? get_site_icon_url() : get_bloginfo('template_directory') . '/img/ohm-logo.png';
             ?>
             <img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>">
           </div>
           <div class="col-sm-10">
-            <!-- Problemstellung: Links zu den social-icon, Vergrößerung, Verkleinerung. Pfade!!! Sie müssen mit \beginnen(!) -->
-             <ul class="list-inline pull-right social-icons">
-             <li> <a href="https://www.facebook.com" target="blank">
-               <img src="<?php echo get_bloginfo('template_directory'); ?> \img\ico\facebook.ico" alt="facebook-icon"  />
-             </a> </li>
-             <li><a href="https://www.twitter.com" target="blank">
-               <img src="<?php echo get_bloginfo('template_directory'); ?> \img\ico\twitter.ico" alt="twitter-icon" />
-             </a></li>
-             <li><a href="https://www.xing.com" target="blank">
-               <img src="<?php echo get_bloginfo('template_directory'); ?> \img\xing.png" alt="xing-icon"/>
-             </a></li>
-             <li><a href="https://www.linkedin.com" target="blank">
-               <img src="<?php echo get_bloginfo('template_directory'); ?> \img\ico\linkedin.ico" alt="linkedin-icon" />
-             </a></li>
-           </ul>
-             <!-- Problemstellung: Impressum ect... -->
-
-        </div>
+            <ul class="list-inline pull-right social-list">
+              <li><a href="https://www.facebook.com" target="_blank"><img src="<?php echo get_bloginfo('template_directory');?>/img/facebook.svg" alt="" /></a></li>
+              <li><a href="https://www.xing.com" target="_blank"><img src="<?php echo get_bloginfo('template_directory');?>/img/xing.svg" /></a></li>
+              <li><a href="https://www.linkedin.com" target="_blank"><img src="<?php echo get_bloginfo('template_directory');?>/img/linkedin.svg" /></a></li>
+            </ul>
+          </div>
       </header>
       <main>
         <h1><?php echo get_bloginfo('name'); ?></h1>
         <h2><?php echo get_bloginfo('description'); ?></h2>
+        <a href="<?php echo get_bloginfo('template_directory');?>/uber.html">
+        <div class="uber">
+          Über uns
+        </div>
+      </a>
       </main>
-      <footer>
-
-
-         <ul class="list-inline Impressum">
-           <li> <a href="https://www.th-nuernberg.de/seitenbaum/impressum/page.html" target="blank">
-             <alt=>Impressum</a> </li>
-             <li> |
-             </li>
-           <li> <a href="https://www.th-nuernberg.de/institutionen/hochschulkommunikation/ohm-shop/agb/page.html" target="blank">
-             <alt=>AGB</a> </li>
-             <li> |
-             </li>
-           <li> <a href="https://www.th-nuernberg.de/institutionen/language-center/ueber-uns/page.html" target="blank">
-             <alt=>Über uns</a> </li>
-         </ul>
-
-
+      <footer class="footer">
+          <ul class="list-inline navbar">
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/impressum.html">Impressum</a></li>
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/datenschutz.html">Datenschutz</a></li>
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/AGB.html">AGB</a></li>
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/kontakt.html">Kontakt</a></li>
+          </ul>
       </footer>
     </div>
 
