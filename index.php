2eecbd316f7b2c0f765680343f527cad756739e2
Marco Lehner <marco-lehner@posteo.net>
2017-01-09 19:53:09 +0100

    various divs added, post-thumbnail with class, moved post-date to bottom

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+ <?php get_header(); ?>
+
 <!-- Start the Loop. -->
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
@@ -10,38 +12,35 @@
     <?php else : ?>
       <div class="post">
       <?php endif; ?>
-
-
       <!-- Display the Title as a link to the Post's permalink. -->
-
-      <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-
-
-      <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
-
-      <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
-
+      <div class="title-wrapper">
+      <h2 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title();?></a></h2>
+      </div>
 
       <!-- Display the Post's content in a div box. -->
 
-      <div class="entry">
+      <div class="header-image gradient">
 
-        <?php // check if the post has a Post Thumbnail assigned to it.
+        <?php // check if the post has a Post Thumbnail assigned to it. If not, add bg-picture of landing page.
         $dir = get_bloginfo('template_directory');
         $file = '/img/p_th_hochschulstandort_stadtansicht.jpg" ';
         if ( has_post_thumbnail() ) {
-          the_post_thumbnail();
+          echo '<img class="featured-image" src="'. get_the_post_thumbnail_url() . '" alt="">';
         }
         else {
-          echo '<img src="'. $dir . $file . 'alt="">';
-        }
-        the_content(); ?>
+          echo '<img class="featured-image" src="'. $dir . $file . 'alt="">';
+        } ?>
       </div>
 
+      <div class="content-wrapper">
+        <div class="content">
+          <?php the_content(); ?>
+        </div>
+      </div>
 
       <!-- Display a comma separated list of the Post's Categories. -->
 
-      <p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
+      <p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' );?> on <?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></p>
     </div> <!-- closes the first div box -->
 
 
@@ -57,3 +56,4 @@
 
     <!-- REALLY stop The Loop. -->
   <?php endif; ?>
+<?php get_footer(); ?>
