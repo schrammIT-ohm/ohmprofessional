b9c0e007e1861413cd0eb74ba4cb3fd20123deab
Marcel Kessler <donhansdampf@posteo.de>
2017-01-12 17:27:08 +0100

    Better Search-Icon-position also responsive

--- searchform.php
+++ searchform.php
@@ -4,7 +4,12 @@
 ?>
 <div>
     <form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
-        <input type="submit" value="">
-        <input placeholder="Suchbegriff eingeben" type="text" value="" name="s">
-    </form>
-</div>
+    	<div class="row">
+    		<div class="col-xs-2 col-sm-1 col-md-3">
+    			<input type="submit" value="">
+    		</div>
+    		<div class="col-xs-10 col-sm-11 col-md-9">
+    			<input placeholder="Suchbegriff eingeben" type="text" value="" name="s">
+    		</div>
+    	</form>
+    </div>
