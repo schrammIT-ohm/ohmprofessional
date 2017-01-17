67f4991dc597d48143f438787c42f4b2ddf6429a
spawnYzn <mail@ckoller.de>
2017-01-17 09:29:19 +0100

    Modified 404 Error Page: *Added Ohm Logo *Added 404 Image *Added Text *Added history.go(-1) *Added Columns as shown in Trello, but cant get them centered

--- 404.php
+++ 404.php
@@ -3,8 +3,53 @@
 
 ?>
 <?php get_header(); ?>
-<h1>404 Fehler</h1>
-<h2>Es ist nicht Deine Schuld.</h2>
-<h2>Es ist das Internet.</h2>
-<p>Die aufgerufene Seite konnte leider nicht gefunden werden.</p>
+<header class="row header">
+    <div class="col-sm-2">
+      <?php
+        // --- Logo
+        // if site-icons is defined: Use the uploaded site icon (Admin->Customizer->Website Infos ->Website Icon)
+        // otherwise use default Logo from ../img/...
+        $logoUrl = has_site_icon() ? get_site_icon_url() : get_bloginfo('template_directory') . '/img/ohm-logo.png';
+      ?>
+      <img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>">
+    </div>
+<div class="col-sm-8">
+<img src="<?php echo get_bloginfo('template_directory');?>/img/soft-404.jpg" alt="404" class="center-block">
+</div>
+</header>
+
+<main>
+<h1 align="middle">Seite nicht gefunden</h1>
+<A HREF="javascript:javascript:history.go(-1)"><p align="middle"><br>Hier klicken um zur Homepage zurückzukehren</p></A>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-2" style="padding-right:20px; border-right: 1px solid #ccc;">
+    <p>Über</p>
+    <p>Kontakt</p>
+    <p>Homepage</p>
+    <p>Impressum</p>
+  </div>
+
+
+    <div class="col-sm-3">
+<?php
+  // =======================================================================//
+  // ! Suche
+  // =======================================================================//
+  get_search_form();
+?>
+</div>
+
+
+  <div class="col-sm-3" style="padding-left:80px; border-left: 1px solid #ccc;">
+  <p>Links</p>
+  <a href="http://www.th-nuernberg.de">www.th-nuernberg.de</a>
+  <a href="http://www.ohm-professional.de">ohm-professional.de</a>
+<p><br><br></p>
+
+</div>
+</div>
+</div>
+</main>
 <?php get_footer(); ?>
