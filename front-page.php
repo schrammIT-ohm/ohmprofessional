743ff622a0348d85bcfef1c56a479049db1bb3ec
mariiee <schwenk.marie@yahoo.de>
2017-01-18 15:29:18 +0100

    änderungen

--- front-page.php
+++ front-page.php
@@ -25,10 +25,10 @@
           </div>
           <div class="col-sm-10">
           <ul class="list-inline pull-right social-icons">
-            <li><a href="https://www.facebook.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/online_social_media_facebook-128.png" width="50px" height="50px alt="Facebook" /></a></li>
-            <li><a href="https://www.twitter.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/1481745744_twitter_online_social_media.png" width="50px" height="50px" alt="Twitter" /></a></a></li>
-            <li><a href="https://www.xing.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/social_media_logo_xing-128.png" width="50px" height="50px" alt="Xing" /></a></li>
-            <li><a href="https://de.linkedin.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory') ?>/img/online_social_media_linked_in-128.png" width="50px" height="50px" alt="Linkedin" /></a></li>
+            <li><a href="https://www.facebook.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/online_social_media_facebook-128.png" alt="Facebook" /></a></li>
+            <li><a href="https://www.xing.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/social_media_logo_xing-128.png" alt="Xing" /></a></li>
+            <li><a href="https://www.twitter.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/1481745744_twitter_online_social_media.png" alt="Twitter" /></a></a></li>
+            <li><a href="https://de.linkedin.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory') ?>/img/online_social_media_linked_in-128.png" alt="Linkedin" /></a></li>
           </ul>
         </div>
         </div>
@@ -38,8 +38,8 @@
         <h2><?php echo get_bloginfo('description'); ?></h2>
         <a href="<?php echo get_bloginfo('template_directory');?>/about.html">
         <div class="about white">
-          Mehr erfahren
-        </div>
+          Über uns
+        </div class="footer"
       </main>
       <footer>
           <ul class="list-inline white">
