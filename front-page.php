d95c51c57bd3276328e5c09ba94f3d270593c303
mariiee <schwenk.marie@yahoo.de>
2017-01-18 13:22:13 +0100

    hifohfih

--- front-page.php
+++ front-page.php
@@ -5,7 +5,6 @@
  * - "front-page.php" is used for the landing-page
  * - "home.php" is used for the blog (a listing of recent posts)
  */
-
 ?>
 
 <?php get_header(); ?>
@@ -21,16 +20,35 @@
               // otherwise use default Logo from ../img/...
               $logoUrl = has_site_icon() ? get_site_icon_url() : get_bloginfo('template_directory') . '/img/ohm-logo.png';
             ?>
-            <img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>">
+            <!-- Bei Klick auf Ohm-Logo immer auf die Landing-Page weiterleiten -->
+            <a href="<?php echo get_bloginfo('template_directory');?>" alt="Startseite"><img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>"></a>
           </div>
-          <div class="col-sm-10"></div>
+          <div class="col-sm-10">
+          <ul class="list-inline pull-right social-icons">
+            <li><a href="https://www.facebook.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/online_social_media_facebook-128.png" width="50px" height="50px alt="Facebook" /></a></li>
+            <li><a href="https://www.twitter.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/1481745744_twitter_online_social_media.png" width="50px" height="50px" alt="Twitter" /></a></a></li>
+            <li><a href="https://www.xing.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory')?>/img/social_media_logo_xing-128.png" width="50px" height="50px" alt="Xing" /></a></li>
+            <li><a href="https://de.linkedin.com/" target="_blank"><img src="<?php echo get_bloginfo('template_directory') ?>/img/online_social_media_linked_in-128.png" width="50px" height="50px" alt="Linkedin" /></a></li>
+          </ul>
+        </div>
         </div>
       </header>
       <main>
         <h1><?php echo get_bloginfo('name'); ?></h1>
         <h2><?php echo get_bloginfo('description'); ?></h2>
+        <a href="<?php echo get_bloginfo('template_directory');?>/about.html">
+        <div class="about white">
+          Mehr erfahren
+        </div>
       </main>
-      <footer></footer>
+      <footer>
+          <ul class="list-inline white">
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/Kontakt.html">Kontakt</a></li>
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/Impressum.html">Impressum</a></li>
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/agb.html">AGB</a></li>
+            <li><a href="<?php echo get_bloginfo('template_directory');?>/Datenschutz.html">Datenschutz</a></li>
+          </ul>
+      </footer>
     </div>
 
     <?php // --- Sidebar for FrontPage: https://developer.wordpress.org/reference/functions/get_sidebar/?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
   <h1>Hallo Welt</h1>
+<span>ich bin ein sinnloser <span>
 
 <?php get_footer(); ?>
