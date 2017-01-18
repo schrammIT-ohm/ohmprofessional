3e1b3331eef28c6e42bb41563b3c9f74304ea36d
Jakob <jakobwa@gmx.de>
2017-01-18 16:25:16 +0100

    front page fertig, scrollbar nicht

--- sidebar-front-page.php
+++ sidebar-front-page.php
@@ -6,32 +6,6 @@
     // =======================================================================//
     get_search_form();
     ?>
-    <div class="wrapper-wrapper">
-        <div class="seminar-wrapper-outer">
-            <?php
-            // =======================================================================//
-            // ! Seminare
-            // =======================================================================//
-            // get all (-1) custom post types "seminare"
-            $args = array(
-                'post_type' => 'seminare',
-                'posts_per_page' => -1
-            );
-            $seminare = new WP_Query( $args );
-            ?>
-            <?php if($seminare->have_posts()): ?>
-                <?php while ( $seminare->have_posts() ) : $seminare->the_post(); ?>
-                    <div class="seminar-wrapper">
-
-                        <a href="<?php the_permalink(); ?>">
-                            <h3 class="seminar"><?php echo get_the_title();  ?></h3>
-                        </a>
-                    </div>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-            <?php endif; ?>
-        </div>
-
         <div class="aside-Liste">
             <h4 style="margin-bottom: 30px; margin-top: 20px"><a href="http://ohm-professional-school.de/home/" target="_blank"><font color="white">Agil Management</font></h4>
             <h4 style="margin-bottom: 30px"><a href="http://ohm-professional-school.de/home/" target="_blank"><font color="white">Design Thinking</font></h4>
