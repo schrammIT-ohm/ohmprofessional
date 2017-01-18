1b9e3c4dca35826d66c565c63ffbeb66b80dd56f
Marcel Kessler <donhansdampf@posteo.de>
2017-01-18 15:49:26 +0100

    Switched to modern Search-Form

--- searchform.php
+++ searchform.php
@@ -4,12 +4,6 @@
 ?>
 <div>
     <form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
-    	<div class="row">
-    		<div class="col-xs-2 col-sm-1 col-md-3">
-    			<input type="submit" value="">
-    		</div>
-    		<div class="col-xs-10 col-sm-11 col-md-9">
-    			<input placeholder="Suchbegriff eingeben" type="text" value="" name="s">
-    		</div>
-    	</form>
-    </div>
+        <input placeholder="Suchbegriff eingeben" type="text" value="" name="s">
+    </form>
+</div>
