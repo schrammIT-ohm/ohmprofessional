79f08269b476d369584ccd235f0d7b80879049d2
Manu <Manu@Manus-MBP-2.home>
2017-01-18 13:16:20 +0100

    Sidebar fertig (soweit machbar)

--- searchform.php
+++ searchform.php
@@ -7,6 +7,6 @@
     <form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
       <img src="<?php echo get_bloginfo('template_directory')?>/img/search-button-without-text-hi.png" alt="Suche-Button" width="30px"/>
 <input type="submit" value="">
-        <input placeholder="Suche ..." type="text" value="" name="s">
+        <input placeholder="Suche beginnen" type="text" value="" name="s">
     </form>
 </div>
