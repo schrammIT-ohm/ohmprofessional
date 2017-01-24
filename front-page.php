b03e71ba77eefa2ee1578e4e85045eb3acc37f6f
Marcel Geldner <marcelgeldner@Marcels-MBP.fritz.box>
2017-01-24 17:51:43 +0100

    Änderung der Anordnung

--- front-page.php
+++ front-page.php
@@ -13,49 +13,69 @@
   <!-- FullScreen Wrapper: content + aside -->
   <div class="fullscreen-wrapper">
     <div class="home-content">
-      <header class="row header">
-          <div class="col-sm-2">
+      <header>
+        <title>Ohm Professional School</title>
+
+          <div class="row">
+             <div class="col-sm-2">
             <?php
               // --- Logo
               // if site-icons is defined: Use the uploaded site icon (Admin->Customizer->Website Infos ->Website Icon)
               // otherwise use default Logo from ../img/...
               $logoUrl = has_site_icon() ? get_site_icon_url() : get_bloginfo('template_directory') . '/img/ohm-logo.png';
             ?>
+            <a href="front-page.php">
             <img class="img-responsive" src="<?php echo $logoUrl; ?>" alt="Logo - <?php echo get_bloginfo('name'); ?>">
+          </a>
           </div>
           <div class="col-sm-10">
-            <ul class="list-inline pull-right social-list">
+            <ul class="list-inline pull-right social-icons hidden-xs">
               <li><a href="https://www.facebook.com" target="_blank">
-                <img src="<?php echo get_bloginfo('template_directory');?>\img\online_social_media_facebook-128.png" alt="facebook-icon"/>
+                <img src="<?php echo get_bloginfo('template_directory');?> \img\online_social_media_facebook-128.png" alt="facebook-icon"/>
               </a></li>
               <li><a href="https://www.twitter.com" target="_blank">
-                <img src="<?php echo get_bloginfo('template_directory');?>\img\1481745744_twitter_online_social_media.png" alt="twitter-icon"/>
+                <img src="<?php echo get_bloginfo('template_directory');?> \img\1481745744_twitter_online_social_media.png" alt="twitter-icon"/>
               </a></li>
               <li><a href="https://www.xing.com" target="_blank">
-                <img src="<?php echo get_bloginfo('template_directory');?>\img\social_media_logo_xing-128.png" alt="xing-icon"/>
+                <img src="<?php echo get_bloginfo('template_directory');?> \img\social_media_logo_xing-128.png" alt="xing-icon"/>
               </a></li>
               <li><a href="https://www.linkedin.com" target="_blank">
-                <img src="<?php echo get_bloginfo('template_directory');?>\img\online_social_media_linked_in-128.png" alt="linkedin-icon"/>
+                <img src="<?php echo get_bloginfo('template_directory');?> \img\online_social_media_linked_in-128.png" alt="linkedin-icon"/>
               </a></li>
             </ul>
           </div>
       </header>
       <main>
+         <center>
+
         <h1>Ohm Professional School</h1>
         <h2>Technische Hochschule Nürnberg</h2>
-        <a href="<?php echo get_bloginfo('template_directory');?>/uber.html">
-        <div class="uber">
-          Über uns
-        </div>
+        <a class="ghost-button-semi-transparent" href="more.php">Mehr erfahren</a>
+          </center>
+          <br>
+        <div class="visible-xs" id="menue18">
+          <ul>
+          <li class="level1"><u>Social Media</u>
+            <ul>
+               <li class="level2"><a href="https://www.facebook.com">Facebook</a></li>
+               <li class="level2"><a href="https://www.twitter.com">Twitter</a></li>
+               <li class="level2"><a href="https://www.xing.com">Xing</a></li>
+               <li class="level2"><a href="https://www.linkedin.com">Linkedin</a></li>
+             </ul>
+           </li>
+         </ul>
+       </div>
       </a>
+
       </main>
-      <footer class="footer">
-          <ul class="list-inline navbar">
-            <li><a href="<?php echo get_bloginfo('template_directory');?>/impressum.html">Impressum</a></li>
-            <li><a href="<?php echo get_bloginfo('template_directory');?>/datenschutz.html">Datenschutz</a></li>
-            <li><a href="<?php echo get_bloginfo('template_directory');?>/AGB.html">AGB</a></li>
-            <li><a href="<?php echo get_bloginfo('template_directory');?>/kontakt.html">Kontakt</a></li>
-          </ul>
+      <footer>
+        <ul class="list-inline pull-left">
+           <li><a href="impressum.php">Impressum</a></li>
+           <li><a href="datenschutz.php">Datenschutz</a></li>
+           <li><a href="agb.php">AGB</a></li>
+           <li><a href="kontakt.php">Kontakt</a></li>
+        </ul>
+    </div>
       </footer>
     </div>
 
