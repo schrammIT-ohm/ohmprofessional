7754f7dc75221fcd3fd9ecbc44c4c347e1a27461
Emre Dagkiran <Emre Dagkiran>
2017-01-25 18:19:39 +0100

    footer mit Impressum, Datenschutz, etc.

--- footer.php
+++ footer.php
@@ -1,3 +1,15 @@
   <?php wp_footer(); ?>
-  </body>
+</body>
 </html>
+      </footer class="navbar-fixed-bottom">
+          <div class="col-sm-10">
+           <ul class="list-inline">
+              <li><a href="#" target="_blank">Impressum</a></li>
+              <li><a href="#" target="_blank">Datenschutz</a></li>
+              <li><a href="#" target="_blank">AGB</a></li>
+              <li><a href="#" target="_blank">Kontakt</a></li>
+            </ul>
+          </div>
+        </footer>
+</html>
+</body>
